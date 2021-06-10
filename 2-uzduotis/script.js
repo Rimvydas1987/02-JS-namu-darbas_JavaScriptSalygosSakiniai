ed17022991a6e63bf4c3df34035b9bce3a71b97c
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-06-10 09:27:28 +0300

    2uzduotis(sandauga)

--- 2-uzduotis/script.js
+++ 2-uzduotis/script.js
@@ -18,16 +18,21 @@ document.getElementById("skaitmenys").innerHTML = "Skaitmenys : " + realDigits;
 
 //suma
 var sumDigits = 0;
-for(var i = 0; i < realDigits.length; i++){
-sumDigits += realDigits[i]
+    for(var i = 0; i < realDigits.length; i++){
+        sumDigits += realDigits[i]
 
 }
 
 document.getElementById("skaitmenusuma").innerHTML = "Skaitmenų suma : " + sumDigits;
 
 //sandauga
+var sanDigits = 1;
+    for (var i = 0; i < realDigits.length; i++) {
+        sanDigits = sanDigits * realDigits[i];
 
+    }
 
+    document.getElementById("skaitmenusandauga").innerHTML = "Skaitmenų sandauga : " + sanDigits;
 
 
 
